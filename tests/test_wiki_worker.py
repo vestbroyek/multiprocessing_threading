da8dd92487564633f158a64e53b28b583a52a3af
Maurits <mcb.westbroek@gmail.com>
2023-10-17 08:34:31 +0100

    Start tests

---
+++ tests/test_wiki_worker.py
@@ -0,0 +1,9 @@
+import unittest
+from workers.wiki_worker import WikiWorker
+
+class TestWikiWorker(unittest.TestCase):
+    def test_get_sp_500(self):
+        worker = WikiWorker()
+        symbols = list(worker.get_sp_500())
+        self.assertGreater(len(symbols), 0)
+        self.assertTrue(all(isinstance(symbol, str) and len(symbol) > 0 for symbol in symbols))
