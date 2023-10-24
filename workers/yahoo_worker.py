66c79b0c92f1380c5c7ad17b47a2127ce2d4dab9
Maurits <mcb.westbroek@gmail.com>
2023-10-24 17:18:06 +0100

    Add thread ID; string replacement

--- workers/yahoo_worker.py
+++ workers/yahoo_worker.py
@@ -4,36 +4,14 @@ import requests
 from lxml import html
 import time
 from queue import Empty
-import datetime
-
-import threading
-import datetime
-import random
-from queue import Empty
+from requests.exceptions import ConnectionError
 
 class YahooScheduler(threading.Thread):
-    """
-    A class for scheduling YahooFinanceWorker tasks on a separate thread.
 
-    Args:
-        input_queue (queue.Queue): A queue of tasks to be processed.
-        output_queue (list or queue.Queue): A list of output queues to put the results in.
-
-    Attributes:
-        input_queue (queue.Queue): A queue of tasks to be processed.
-        output_queues (list): A list of output queues to put the results in.
-
-    Methods:
-        run(): The main method of the thread. Retrieves tasks from the input queue, processes them using
-              YahooFinanceWorker, and puts the results in the output queues.
-    """
-    def __init__(self, input_queue, output_queue, **kwargs):
+    def __init__(self, id, input_queue, **kwargs):
         super().__init__(**kwargs)
+        self.id = id
         self.input_queue = input_queue
-        temp_queue = output_queue
-        if not isinstance(output_queue, list):
-            temp_queue = [output_queue]
-        self.output_queues = temp_queue
         self.start()
 
     def run(self):
@@ -50,66 +28,33 @@ class YahooScheduler(threading.Thread):
             if task == 'DONE': # If the task is 'DONE', stop the thread
                 break
 
-            yahoo_worker= YahooFinanceWorker(task) # Create a YahooFinanceWorker object for the task
-            price = yahoo_worker.run() # Retrieve the stock price using the YahooFinanceWorker object
-            for output_queue in self.output_queues: # Put the output values into each output queue
-                output_values = (task, price, datetime.datetime.utcnow())
-                output_queue.put(output_values)
-            time.sleep(random.random()) # Sleep for a random amount of time to simulate processing time
+            yahoo_worker = YahooFinanceWorker(thread_id = self.id, symbol=task) 
+            price = yahoo_worker.get_price()
+            print(self.id, " ", task, " ", price)
+            time.sleep(random.random())
 
 
-class YahooFinanceWorker(threading.Thread):
-    """
-    A class representing a worker that extracts company information from Yahoo Finance.
+class YahooFinanceWorker():
 
-    Attributes:
-    -----------
-    symbol : str
-        The stock symbol of the company to extract information for.
-    url : str
-        The URL of the Yahoo Finance page for the given stock symbol.
-    """
-
-    def __init__(self, symbol: str, **kwargs):
-        """
-        Initializes a new instance of the YahooFinanceWorker class.
-
-        Parameters:
-        -----------
-        symbol : str
-            The stock symbol of the company to extract information for.
-        **kwargs : dict
-            Additional keyword arguments to pass to the threading.Thread constructor.
-        """
+    def __init__(self, thread_id, symbol: str, **kwargs):
         super().__init__(**kwargs)
+        self.thread_id = thread_id
         self.symbol = symbol
         base_url = "https://finance.yahoo.com/quote/"
         self.url = base_url + symbol
-        self.start()
 
     def _extract_company_info(self, page_html: str) -> float:
-        """
-        Extracts the current price of the company from the given HTML page.
-
-        Parameters:
-        -----------
-        page_html : str
-            The HTML content of the Yahoo Finance page for the given stock symbol.
-
-        Returns:
-        --------
-        float
-            The current price of the company.
-        """
         tree = html.fromstring(page_html)
-        company_price = float(tree.xpath("//*[@id=\"quote-header-info\"]/div[3]/div[1]/div[1]/fin-streamer[1]")[0].text)
-        return company_price
+        try:
+            company_price = tree.xpath("//*[@id=\"quote-header-info\"]/div[3]/div[1]/div[1]/fin-streamer[1]")[0].text
+            if company_price:
+                company_price = company_price.replace(',', '')
+                return company_price
+        except ConnectionError:
+            print(f"Thread ID {self.thread_id} raised a connection error")
 
-    def run(self):
-        """
-        Runs the worker thread, extracting the current price of the company from the Yahoo Finance page.
-        """
-        time.sleep(0.5)
+    def get_price(self):
+        time.sleep(0.2)
         response = requests.get(self.url, headers={"User-Agent":"Mozilla/5.0"})
         if response.status_code == 200:
             price = self._extract_company_info(response.text)
