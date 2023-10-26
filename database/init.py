1fe28d997a8a3d953168d3ea088cc912d7775e0c
Maurits <mcb.westbroek@gmail.com>
2023-10-26 16:42:11 +0100

    Rename to .py

--- database/init.py
+++ database/init.py
@@ -1,7 +1,7 @@
 import sqlite3
 
-# Create a connection to the database
-conn = sqlite3.connect('stocks.db')
+# Create a connection to the database
+conn = sqlite3.connect('./stocks.db')
 
 # Create a table
 conn.execute('''CREATE TABLE prices (id serial primary key, symbol text, price real, extraction_time timestamp)''')
